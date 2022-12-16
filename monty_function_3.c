17327a26d6888e16b5d00e72b622a2a4979e4911
Barackhalogen <barackhalogen@gmail.com>
2022-12-15 20:39:35 -0800

    monty_function_3.c

--- monty_function_3.c
+++ monty_function_3.c
@@ -4,11 +4,10 @@ void monty_nop(stack_t **stack, unsigned int line_number);
 void monty_pchar(stack_t **stack, unsigned int line_number);
 void monty_pstr(stack_t **stack, unsigned int line_number);
 
-
 /**
  * monty_nop - Does absolutely nothing for the Monty opcode 'nop'.
  * @stack: A pointer to the top mode node of a stack_t linked list.
- * @line_number: The current working line number of a Monty bytecodes file
+ * @line_number: The current working line number of a Monty bytecodes file.
  */
 void monty_nop(stack_t **stack, unsigned int line_number)
 {
